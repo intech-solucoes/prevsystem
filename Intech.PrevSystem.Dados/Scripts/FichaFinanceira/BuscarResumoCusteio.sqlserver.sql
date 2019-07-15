9de2db34253c4782d12b7f65fbd1aaf0d18d3d18
Rony <ronymmoura@gmail.com>
2019-07-15 16:19:49 -0300

    Ajuste na query BuscarResumoCusteio

--- Intech.PrevSystem.Dados/Scripts/FichaFinanceira/BuscarResumoCusteio.sqlserver.sql
+++ Intech.PrevSystem.Dados/Scripts/FichaFinanceira/BuscarResumoCusteio.sqlserver.sql
@@ -25,11 +25,11 @@ WHERE FF.CD_FUNDACAO = @CD_FUNDACAO
   AND FF.NUM_INSCRICAO = @NUM_INSCRICAO
   AND FF.CD_PLANO = @CD_PLANO
   AND TC.COD_AGRUPADOR_WEB IN ('ADMPA', 'ADMPT', 'RIPA', 'RIPT')
-  AND (FF.ANO_REF * 12 + FF.MES_REF) = (SELECT MAX(FF2.ANO_REF * 12 + FF2.MES_REF)
-                                          FROM CC_FICHA_FINANCEIRA FF2 
+  AND (FF.ANO_REF * 12 + FF.MES_REF) = (SELECT MAX(YEAR(FF2.DT_FECHAMENTO) * 12 + MONTH(FF2.DT_FECHAMENTO))
+                                          FROM CC_FICHA_FECHAMENTO FF2 
                                          WHERE FF2.CD_FUNDACAO = FF.CD_FUNDACAO
                                            AND FF2.NUM_INSCRICAO = FF.NUM_INSCRICAO
-                                           AND FF2.CD_PLANO = FF.CD_PLANO)        
+                                           AND FF2.CD_PLANO = FF.CD_PLANO)
 GROUP BY FF.ANO_REF,
          FF.MES_REF, 
          TC.COD_AGRUPADOR_WEB

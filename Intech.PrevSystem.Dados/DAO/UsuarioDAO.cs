017611e498b3d4a2b20f83dd5255a607d3cf6d7e
Rony <ronymmoura@gmail.com>
2019-07-23 02:03:08 -0300

    Implementando troca de senha no LGPD

--- Intech.PrevSystem.Dados/DAO/UsuarioDAO.cs
+++ Intech.PrevSystem.Dados/DAO/UsuarioDAO.cs
@@ -18,7 +18,7 @@ namespace Intech.PrevSystem.Dados.DAO
 			try
 			{
 				if(AppSettings.IS_SQL_SERVER_PROVIDER)
-					return Conexao.QuerySingleOrDefault<UsuarioEntidade>("SELECT TOP 1 * FROM WEB_USUARIO WHERE NOM_LOGIN = @NOM_LOGIN", new { NOM_LOGIN });
+					return Conexao.QuerySingleOrDefault<UsuarioEntidade>("SELECT TOP 1 *  FROM WEB_USUARIO  WHERE NOM_LOGIN = @NOM_LOGIN", new { NOM_LOGIN });
 				else if(AppSettings.IS_ORACLE_PROVIDER)
 					return Conexao.QuerySingleOrDefault<UsuarioEntidade>("SELECT * FROM WEB_USUARIO WHERE NOM_LOGIN=:NOM_LOGIN AND ROWNUM <= 1 ", new { NOM_LOGIN });
 				else
@@ -35,7 +35,7 @@ namespace Intech.PrevSystem.Dados.DAO
 			try
 			{
 				if(AppSettings.IS_SQL_SERVER_PROVIDER)
-					return Conexao.QuerySingleOrDefault<UsuarioEntidade>("SELECT TOP 1 * FROM WEB_USUARIO WHERE NOM_LOGIN = @NOM_LOGIN   AND PWD_USUARIO = @PWD_USUARIO", new { NOM_LOGIN, PWD_USUARIO });
+					return Conexao.QuerySingleOrDefault<UsuarioEntidade>("SELECT TOP 1 *  FROM WEB_USUARIO  WHERE NOM_LOGIN = @NOM_LOGIN    AND PWD_USUARIO = @PWD_USUARIO", new { NOM_LOGIN, PWD_USUARIO });
 				else if(AppSettings.IS_ORACLE_PROVIDER)
 					return Conexao.QuerySingleOrDefault<UsuarioEntidade>("SELECT * FROM WEB_USUARIO WHERE NOM_LOGIN=:NOM_LOGIN AND PWD_USUARIO=:PWD_USUARIO AND ROWNUM <= 1 ", new { NOM_LOGIN, PWD_USUARIO });
 				else

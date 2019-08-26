9b208659ea656749c1300dbe5099f33996222a7b
Rony <ronymmoura@gmail.com>
2019-08-26 17:01:28 -0300

    [Sabesprev] Removendo busca de contracheque do base

--- Sabesprev/Intech.PrevSystem.Sabesprev.Api/Controllers/FichaFinanceiraAssistidoController.cs
+++ Sabesprev/Intech.PrevSystem.Sabesprev.Api/Controllers/FichaFinanceiraAssistidoController.cs
@@ -2,6 +2,7 @@
 using Intech.PrevSystem.API;
 using Intech.PrevSystem.Entidades;
 using Intech.PrevSystem.Negocio.Proxy;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,7 +13,58 @@ using System.Linq;
 namespace Intech.PrevSystem.Sabesprev.Api.Controllers
 {
     [Route(RotasApi.FichaFinanceiraAssistido)]
-    public class FichaFinanceiraAssistidoController : BaseFichaFinanceiraAssistidoController
+    public class FichaFinanceiraAssistidoController : BaseController
     {
+        [HttpGet("datasPorPlano/{cdPlano}")]
+        [Authorize("Bearer")]
+        public IActionResult BuscarDatas(string cdPlano)
+        {
+            try
+            {
+                //var quantidadeMesesContraCheque = 18;
+                //var dtReferencia = DateTime.Today.PrimeiroDiaDoMes().AddMonths(-quantidadeMesesContraCheque);
+
+                List<FichaFinanceiraAssistidoEntidade> datas;
+
+                if (Pensionista)
+                    datas = new FichaFinanceiraAssistidoProxy().BuscarDatasPorRecebedor(CdFundacao, CdEmpresa, Matricula, SeqRecebedor, cdPlano).ToList();
+                else
+                    datas = new FichaFinanceiraAssistidoProxy().BuscarDatas(CdFundacao, CdEmpresa, Matricula, cdPlano).ToList();
+
+                datas.ForEach(x =>
+                {
+                    x.IsAbonoAnual = x.CD_TIPO_FOLHA == "3";
+                });
+
+                return Json(datas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("porPlanoReferenciaTipoFolha/{cdPlano}/{referencia}/{cdTipoFolha}")]
+        [Authorize("Bearer")]
+        public IActionResult BuscarPorDataReferencia(string cdPlano, string referencia, string cdTipoFolha)
+        {
+            try
+            {
+                var dataReferencia = DateTime.ParseExact(referencia, "dd.MM.yyyy", new CultureInfo("pt-BR"));
+
+                dynamic rubricas;
+
+                if (Pensionista)
+                    rubricas = new FichaFinanceiraAssistidoProxy().BuscarRubricasPorFundacaoEmpresaMatriculaPlanoReferencia(CdFundacao, CdEmpresa, Matricula, cdPlano, dataReferencia, cdTipoFolha, SeqRecebedor);
+                else
+                    rubricas = new FichaFinanceiraAssistidoProxy().BuscarRubricasPorFundacaoEmpresaMatriculaPlanoReferencia(CdFundacao, CdEmpresa, Matricula, cdPlano, dataReferencia, cdTipoFolha);
+
+                return Json(rubricas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }

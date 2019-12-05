b6e42e8cf4989cb04107f04ddcc303e99fef4bcc
Rony Moura <ronymmoura@gmail.com>
2019-12-05 19:44:55 -0300

    Incluindo IPV4 e IPV6 na adesão

--- Intech.PrevSystem.Entidades/AdesaoEntidade.cs
+++ Intech.PrevSystem.Entidades/AdesaoEntidade.cs
@@ -60,6 +60,8 @@ namespace Intech.PrevSystem.Entidades
 		[Write(false)] public AdesaoContribEntidade Contrib { get; set; }
 		[Write(false)] public AdesaoPlanoEntidade Plano { get; set; }
 		[Write(false)] public List<AdesaoDocumentoEntidade> Documentos { get; set; }
+		[Write(false)] public string IPV4 { get; set; }
+		[Write(false)] public string IPV6 { get; set; }
         
     }
 }

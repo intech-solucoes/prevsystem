0bf869a346dadc95bb55270ea8bd3da506e77c6d
Rony Moura <ronymmoura@gmail.com>
2020-02-11 14:38:39 -0300

    [Previrb] Correção em query de processo de beneficio e ficha financeira

--- Intech.PrevSystem.Dados/Scripts/FichaFinanceira/BuscarContribuicaoPorInscricaoAnoComp.sqlserver.sql
+++ Intech.PrevSystem.Dados/Scripts/FichaFinanceira/BuscarContribuicaoPorInscricaoAnoComp.sqlserver.sql
@@ -27,11 +27,13 @@ WHERE FF.CD_FUNDACAO   = '01'
                                          WHERE FF2.CD_FUNDACAO = FF.CD_FUNDACAO
                                            AND FF2.NUM_INSCRICAO = FF.NUM_INSCRICAO
                                            AND FF2.CD_PLANO = FF.CD_PLANO
+                                           AND FF2.MES_COMP <> '13'
 										   AND (FF2.ANO_REF * 12 + FF2.MES_REF) = (SELECT MAX(FF2.ANO_REF * 12 + FF2.MES_REF)
                                                                                      FROM CC_FICHA_FINANCEIRA FF2 
                                                                                     WHERE FF2.CD_FUNDACAO = FF.CD_FUNDACAO
                                                                                       AND FF2.NUM_INSCRICAO = FF.NUM_INSCRICAO
-                                                                                      AND FF2.CD_PLANO = FF.CD_PLANO))
+                                                                                      AND FF2.CD_PLANO = FF.CD_PLANO
+                                                                                      AND FF2.MES_COMP <> '13'))
   AND (FVC.ANO_REF * 12 + FVC.MES_REF) = (SELECT MAX(FVC2.ANO_REF * 12 + FVC2.MES_REF)
                                           FROM TB_FAIXA_VALOR_CONTRIB FVC2 
                                          WHERE FVC2.CD_FUNDACAO = FVC.CD_FUNDACAO

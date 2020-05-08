0335708f957a2f10be4d5637e6e06f30695846fa
Rony Moura <ronymmoura@gmail.com>
2020-05-08 16:50:34 -0300

    [Sabesprev] Criando declaração para fins de IR

---
+++ Intech.PrevSystem.Dados/Scripts/FuncionarioNP/BuscarPorFundacaoCpf.sql
@@ -0,0 +1,12 @@
+﻿/*Config
+    Retorno
+        -FuncionarioNPEntidade
+    Parametros
+        -CD_FUNDACAO:string
+        -CPF_CGC:string
+*/
+
+SELECT *
+FROM CS_FUNCIONARIO_NP
+WHERE CD_FUNDACAO = @CD_FUNDACAO
+  AND CPF_CGC = @CPF_CGC

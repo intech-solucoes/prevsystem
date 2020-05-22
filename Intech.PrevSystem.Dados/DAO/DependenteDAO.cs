df035a378f4a3f09daf317b8194a3e5f2f659fbf
sergiochacal <sergio.chacal@gmail.com>
2020-05-22 14:23:29 -0300

    ajustes na entidade planos vinculados para trazer os dados para os participantes DESLIGADOS

--- Intech.PrevSystem.Dados/DAO/DependenteDAO.cs
+++ Intech.PrevSystem.Dados/DAO/DependenteDAO.cs
@@ -112,5 +112,22 @@ namespace Intech.PrevSystem.Dados.DAO
 			}
 		}
 
+		public virtual int ObterUltimoSequencial(string CD_FUNDACAO, string NUM_INSCRICAO)
+		{
+			try
+			{
+				if (AppSettings.IS_SQL_SERVER_PROVIDER)
+					return Conexao.QuerySingleOrDefault<int>("SELECT MAX(NUM_SEQ_DEP) AS NUM_SEQ_DEP   FROM CS_DEPENDENTE  WHERE CD_FUNDACAO = @CD_FUNDACAO    AND NUM_INSCRICAO = @NUM_INSCRICAO", new { CD_FUNDACAO, NUM_INSCRICAO });
+				else if (AppSettings.IS_ORACLE_PROVIDER)
+					return Conexao.QuerySingleOrDefault<int>("SELECT MAX(NUM_SEQ_DEP) AS NUM_SEQ_DEP FROM CS_DEPENDENTE WHERE CD_FUNDACAO=:CD_FUNDACAO AND NUM_INSCRICAO=:NUM_INSCRICAO", new { CD_FUNDACAO, NUM_INSCRICAO });
+				else
+					throw new Exception("Provider não suportado!");
+			}
+			finally
+			{
+				Conexao.Close();
+			}
+		}
+
 	}
 }

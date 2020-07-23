87f06285c4b133bb195ce8a5099c63dec26805e1
Rony Moura <ronymmoura@gmail.com>
2020-07-23 13:22:21 -0300

    [Regius] Ajuste na adesao

--- Intech.PrevSystem.Negocio/Proxy/AdesaoProxy.cs
+++ Intech.PrevSystem.Negocio/Proxy/AdesaoProxy.cs
@@ -1,8 +1,17 @@
 ﻿using Intech.PrevSystem.Dados.DAO;
+using Intech.PrevSystem.Entidades;
+using System;
 
 namespace Intech.PrevSystem.Negocio.Proxy
 {
 	public class AdesaoProxy : AdesaoDAO
 	{
-	}
+        public override long Inserir(AdesaoEntidade entidade)
+        {
+            entidade.COD_TELEFONE_CELULAR = entidade.COD_TELEFONE_CELULAR.LimparMascara();
+            entidade.COD_TELEFONE_FIXO = entidade.COD_TELEFONE_FIXO.LimparMascara();
+
+            return base.Inserir(entidade);
+        }
+    }
 }

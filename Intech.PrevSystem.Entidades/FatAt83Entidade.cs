0fc021e0f80f576109bd89f30505dba1c92853c7
Yuka <maidyuka@gmail.com>
2020-12-03 13:18:14 -0300

    [Faelce / Sabesprev] Ajuste nas Buscas do Extrato de Assistido

---
+++ Intech.PrevSystem.Entidades/FatAt83Entidade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Dapper.Contrib.Extensions;
+
+namespace Intech.PrevSystem.Entidades
+{
+	[Table("GB_FAT_AT83")]
+	public class FatAt83Entidade
+	{
+		public decimal? IDADE { get; set; }
+		public decimal? VALOR_QX { get; set; }
+		public decimal? VALOR_LX { get; set; }
+		public decimal? VALOR_DX { get; set; }
+	}
+}

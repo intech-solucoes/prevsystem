7b6a38ba07cc6e6c678ebefabef2e67fa24bd025
Yuka <maidyuka@gmail.com>
2020-12-29 15:23:32 -0300

    [Geral] Ajuste nas Buscas dos Documentos

--- Intech.PrevSystem.Dados/DAO/DocumentoDAO.cs
+++ Intech.PrevSystem.Dados/DAO/DocumentoDAO.cs
@@ -4,12 +4,15 @@ using Intech.Lib.Web;
 using Intech.PrevSystem.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 
 namespace Intech.PrevSystem.Dados.DAO
 {
 	public abstract class DocumentoDAO : BaseDAO<DocumentoEntidade>
 	{
+		public DocumentoDAO (IDbTransaction tx = null) : base(tx) { }
+
 		public virtual DocumentoEntidade BuscarPorOid(decimal OID_DOCUMENTO)
 		{
 			try
@@ -23,7 +26,8 @@ namespace Intech.PrevSystem.Dados.DAO
 			}
 			finally
 			{
-				Conexao.Close();
+				if(Transaction == null)
+					Conexao.Close();
 			}
 		}
 
@@ -40,7 +44,8 @@ namespace Intech.PrevSystem.Dados.DAO
 			}
 			finally
 			{
-				Conexao.Close();
+				if(Transaction == null)
+					Conexao.Close();
 			}
 		}
 
@@ -57,7 +62,8 @@ namespace Intech.PrevSystem.Dados.DAO
 			}
 			finally
 			{
-				Conexao.Close();
+				if(Transaction == null)
+					Conexao.Close();
 			}
 		}
 
@@ -74,7 +80,26 @@ namespace Intech.PrevSystem.Dados.DAO
 			}
 			finally
 			{
-				Conexao.Close();
+				if(Transaction == null)
+					Conexao.Close();
+			}
+		}
+
+		public virtual List<DocumentoEntidade> BuscarPorPastaPlanoOrderByTxtTitulo(decimal? OID_DOCUMENTO_PASTA, string CD_PLANO, string ORDER_CRITERIA)
+		{
+			try
+			{
+				if (AppSettings.IS_SQL_SERVER_PROVIDER)
+					return Conexao.Query<DocumentoEntidade>("SELECT *   FROM WEB_DOCUMENTO D LEFT JOIN WEB_DOCUMENTO_PLANO DP   ON D.OID_DOCUMENTO = DP.OID_DOCUMENTO   LEFT JOIN TB_PLANOS P   ON DP.CD_PLANO = P.CD_PLANO   WHERE ((OID_DOCUMENTO_PASTA = @OID_DOCUMENTO_PASTA)     OR (@OID_DOCUMENTO_PASTA IS NULL AND OID_DOCUMENTO_PASTA IS NULL))     AND ((P.CD_PLANO = @CD_PLANO  OR DP.CD_PLANO IS NULL)     OR (@CD_PLANO IS NULL AND P.CD_PLANO IS NULL))     ORDER BY D.TXT_TITULO", new { OID_DOCUMENTO_PASTA, CD_PLANO, ORDER_CRITERIA }).ToList();
+				else if (AppSettings.IS_ORACLE_PROVIDER)
+					return Conexao.Query<DocumentoEntidade>("SELECT * FROM WEB_DOCUMENTO  D  LEFT JOIN WEB_DOCUMENTO_PLANO   DP  ON D.OID_DOCUMENTO=DP.OID_DOCUMENTO LEFT JOIN TB_PLANOS   P  ON DP.CD_PLANO=P.CD_PLANO WHERE ((OID_DOCUMENTO_PASTA=:OID_DOCUMENTO_PASTA) OR (:OID_DOCUMENTO_PASTA IS NULL  AND OID_DOCUMENTO_PASTA IS NULL )) AND ((P.CD_PLANO=:CD_PLANO OR DP.CD_PLANO IS NULL ) OR (:CD_PLANO IS NULL  AND P.CD_PLANO IS NULL )) ORDER BY D.TXT_TITULO", new { OID_DOCUMENTO_PASTA, CD_PLANO, ORDER_CRITERIA }).ToList();
+				else
+					throw new Exception("Provider não suportado!");
+			}
+			finally
+			{
+				if(Transaction == null)
+					Conexao.Close();
 			}
 		}
 
@@ -91,7 +116,8 @@ namespace Intech.PrevSystem.Dados.DAO
 			}
 			finally
 			{
-				Conexao.Close();
+				if(Transaction == null)
+					Conexao.Close();
 			}
 		}
 
--- Intech.PrevSystem.Negocio/Proxy/DocumentoProxy.cs
+++ Intech.PrevSystem.Negocio/Proxy/DocumentoProxy.cs
@@ -1,8 +1,10 @@
 ﻿using Intech.PrevSystem.Dados.DAO;
+using System.Data;
 
 namespace Intech.PrevSystem.Negocio.Proxy
 {
 	public class DocumentoProxy : DocumentoDAO
 	{
+		public DocumentoProxy (IDbTransaction tx = null) : base(tx) { }
 	}
 }

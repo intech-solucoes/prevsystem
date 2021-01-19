80a1445c30b12ee62c0e4f6a1e17804c5349bf2a
Yuka <maidyuka@gmail.com>
2021-01-19 19:05:59 -0300

    [Sabesprev] Add Busca de Diferido

--- Intech.PrevSystem.Entidades/Extensoes/PlanoVinculadoExtensoes.cs
+++ Intech.PrevSystem.Entidades/Extensoes/PlanoVinculadoExtensoes.cs
@@ -8,5 +8,6 @@ namespace System
         public static bool IsDesligado(this PlanoVinculadoEntidade plano) => plano.CD_CATEGORIA == "2";
         public static bool IsAutopatrocinio(this PlanoVinculadoEntidade plano) => plano.CD_CATEGORIA == "3";
         public static bool IsAssistido(this PlanoVinculadoEntidade plano) => plano.CD_CATEGORIA == "4";
+        public static bool IsDiferido(this PlanoVinculadoEntidade plano) => plano.CD_CATEGORIA == "6";
     }
 }

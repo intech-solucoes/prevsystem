87fed5fce976e5b618c18013f696cb29687841d8
Yuka <maidyuka@gmail.com>
2021-01-27 14:51:14 -0300

    [Faelce] Ajuste na Busca das Contribuicoes de Auto Patrocinado

--- Intech.PrevSystem.Dados/Scripts/FichaFinanceira/BuscarContribuicaoPorFundacaoInscricaoPlanoTipoContribuicaoFixa.sql
+++ Intech.PrevSystem.Dados/Scripts/FichaFinanceira/BuscarContribuicaoPorFundacaoInscricaoPlanoTipoContribuicaoFixa.sql
@@ -8,15 +8,16 @@
 		-CD_PLANO:string
 */
 
-SELECT FF.*
-FROM CC_FICHA_FINANCEIRA FF
-WHERE FF.CD_FUNDACAO = @CD_FUNDACAO
-  AND FF.NUM_INSCRICAO = @NUM_INSCRICAO
-  AND FF.CD_PLANO = @CD_PLANO
-  AND FF.CD_TIPO_CONTRIBUICAO IN ('65', '25', '28')
-  AND FF.ANO_COMP * 13 + FF.MES_COMP = (SELECT MAX(FF2.ANO_COMP * 13 + FF2.MES_COMP) 
+SELECT FF.*  
+FROM CC_FICHA_FINANCEIRA FF  
+WHERE FF.CD_FUNDACAO = @CD_FUNDACAO    
+  AND FF.NUM_INSCRICAO = @NUM_INSCRICAO    
+  AND FF.CD_PLANO = @CD_PLANO    
+  AND FF.CD_TIPO_CONTRIBUICAO IN ('65', '25', '28')    
+  AND FF.ANO_COMP * 13 + FF.MES_COMP = (SELECT MAX(FF2.ANO_COMP * 13 + FF2.MES_COMP)                                           
                                         FROM CC_FICHA_FINANCEIRA FF2
-                                        WHERE FF2.CD_FUNDACAO = FF.CD_FUNDACAO
-                                          AND FF2.NUM_INSCRICAO = FF.NUM_INSCRICAO
-                                          AND FF2.CD_PLANO = FF.CD_PLANO
+										WHERE FF2.CD_FUNDACAO = FF.CD_FUNDACAO                                            
+										  AND FF2.NUM_INSCRICAO = FF.NUM_INSCRICAO                                            
+										  AND FF2.CD_PLANO = FF.CD_PLANO  										  
+										  AND FF2.CD_TIPO_CONTRIBUICAO IN ('65', '25', '28')    
 										  AND FF2.MES_COMP <> 13)

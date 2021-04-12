3ae42c3aa6370f766f84de0fc02304ebae929745
Yuka <maidyuka@gmail.com>
2021-04-12 18:21:22 -0300

    [Geral] Add Busca de Doc Por Nome Local

--- Intech.PrevSystem.Dados/DAO/ArquivoUploadDAO.cs
+++ Intech.PrevSystem.Dados/DAO/ArquivoUploadDAO.cs
@@ -61,6 +61,24 @@ namespace Intech.PrevSystem.Dados.DAO
 			}
 		}
 
+		public virtual List<ArquivoUploadEntidade> BuscarPorNomeLocal(string NOM_ARQUIVO_LOCAL)
+		{
+			try
+			{
+				if (AppSettings.IS_SQL_SERVER_PROVIDER)
+					return Conexao.Query<ArquivoUploadEntidade>("SELECT * FROM TBG_ARQUIVO_UPLOAD WHERE    NOM_ARQUIVO_LOCAL = @NOM_ARQUIVO_LOCAL", new { NOM_ARQUIVO_LOCAL }).ToList();
+
+                else if (AppSettings.IS_ORACLE_PROVIDER)
+					return Conexao.Query<ArquivoUploadEntidade>("SELECT * FROM TBG_ARQUIVO_UPLOAD WHERE NOM_ARQUIVO_LOCAL=:NOM_ARQUIVO_LOCAL", new { NOM_ARQUIVO_LOCAL }).ToList();
+				else
+					throw new Exception("Provider não suportado!");
+			}
+			finally
+			{
+				Conexao.Close();
+			}
+		}
+
 		public virtual ArquivoUploadEntidade BuscarPorOid(decimal OID_ARQUIVO_UPLOAD)
 		{
 			try
---
+++ Intech.PrevSystem.Dados/Scripts/ArquivoUpload/BuscarPorNomeLocal.sql
@@ -0,0 +1,11 @@
+﻿/*Config
+	RetornaLista
+    Retorno
+        -ArquivoUploadEntidade
+    Parametros
+        -NOM_ARQUIVO_LOCAL:string
+*/
+
+SELECT * FROM TBG_ARQUIVO_UPLOAD
+WHERE
+    NOM_ARQUIVO_LOCAL = @NOM_ARQUIVO_LOCAL
